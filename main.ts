807c4b0a771b6ca0dc417ae26c9e9ed7bb16f8f7
Max_ <mrom06@ya.ru>
2019-06-15 19:39:05 +0300

    Update

--- main.ts
+++ main.ts
@@ -8,19 +8,23 @@
 // - M - - - - - A A A X X X - -
 // - - - - - - - - - - - - - - -
 
-import "../core/output"
+// import '../core/output';
+// import '../color-sensor/color';
 
 interface electronicSettings {
-  rightMotor:  motors.Motor
+  rightMotor: motors.Motor;
+  leftMotor: motors.Motor;
+  rightSensor: sensors.ColorSensor;
+  leftSensor: sensors.ColorSensor;
 }
 
 interface RobotSettings {
-
+  electronic: electronicSettings;
 }
 
 class Robot {
-  constructor(){
-
+  settings: RobotSettings
+  constructor(settings: RobotSettings) {
+    this.settings = settings;
   }
 }
-
