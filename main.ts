bd6ceb9bffec2f808161d129601710df2f690d98
Max_ <mrom06@ya.ru>
2019-06-15 19:02:59 +0300

    Ts setup

--- main.ts
+++ main.ts
@@ -7,6 +7,17 @@
 // - - M - - - - - - - - - - - -
 // - M - - - - - A A A X X X - -
 // - - - - - - - - - - - - - - -
+
+import "../core/output"
+
+interface electronicSettings {
+  rightMotor:  motors.Motor
+}
+
+interface RobotSettings {
+
+}
+
 class Robot {
   constructor(){
 
