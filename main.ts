ba89071f2074b49ad26d1367c86947363a92b55e
Max_ <mrom06@ya.ru>
2019-06-16 09:40:01 +0300

    Motore mechanics

--- main.ts
+++ main.ts
@@ -10,12 +10,15 @@
 
 // import '../core/output';
 // import '../color-sensor/color';
+// import '../base/shims';
+// function pauseUntil(condition: () => boolean, timeOut?: number): void {}
 
 interface electronicSettings {
   rightMotor: motors.Motor;
   leftMotor: motors.Motor;
   rightSensor: sensors.ColorSensor;
   leftSensor: sensors.ColorSensor;
+  speed: number;
 }
 
 interface constructionSettings {
@@ -46,9 +49,58 @@ interface RobotSettings {
   line: lineSettings;
 }
 
+enum Side {
+  Right,
+  Left
+}
+
 class Robot {
   settings: RobotSettings;
   constructor(settings: RobotSettings) {
     this.settings = settings;
+  }
+
+  readDataFromSensor(side: Side): number {
+    let sensor =
+      side == Side.Right
+        ? this.settings.electronic.rightSensor
+        : this.settings.electronic.leftSensor;
+
+    let errK =
+      side == Side.Right ? this.settings.error.kRightSensor : this.settings.error.kLeftSensor;
+    return sensor.reflectedLight() * errK;
+  }
+
+  runMotor(side: Side, speed: number) {
+    let motor =
+      side == Side.Right
+        ? this.settings.electronic.rightMotor
+        : this.settings.electronic.leftMotor;
+    let errK =
+      side == Side.Right ? this.settings.error.kRightWheel : this.settings.error.kLeftWheel;
+    motor.run(speed * errK);
+  }
+
+  stopMotor(side: Side) {
+    let motor =
+      side == Side.Right
+        ? this.settings.electronic.rightMotor
+        : this.settings.electronic.leftMotor;
+    motor.stop()
+  }
+
+  moveWheel(side: Side, speed: number, until: () => boolean = () => false, stop: boolean = true) {
+    this.runMotor(side, speed);
+    pauseUntil(() => {
+      return until();
+    });
+    if(stop)this.stopMotor(side)
+  }
+
+  untilSeconds(time: number): () => boolean {
+    let startTime = control.millis()
+    return ()=>{
+      return (control.millis() - startTime) < time
+    }
   }
 }
